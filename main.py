0ab764e6d34363f7ac0127f29838995bd2f4c724
McCarthy0501 <rocr1105@gmail.com>
2026-02-01 17:27:30 -0400

    estructura incializada, main listo para arrancar

---
+++ main.py
@@ -0,0 +1,18 @@
+import logic # procesos y logica de la app
+import database # conexicon de base de datos 
+import gui # interfaz de usuario 
+
+
+
+def main () :
+    #llamamos a cada modulo 
+    
+    print("inicializando progrma ... ")
+    pass
+
+
+
+#punto final del script 
+if __name__ == "__main__":
+    main() # llamada a la funcion principal 
+    pass
